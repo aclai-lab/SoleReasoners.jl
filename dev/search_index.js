99bf2e09a85c59b76f3d80f19d22c5c9713a1aab
Documenter.jl <documenter@juliadocs.github.io>
2024-01-14 13:50:11 +0000

    build based on 8aa5aa6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#SoleReasoners.jl","page":"Home","title":"SoleReasoners.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SoleReasoners.jl","category":"page"}]
+}
