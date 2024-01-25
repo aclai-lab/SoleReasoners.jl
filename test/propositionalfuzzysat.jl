cbde16a77b6244fad68a108b9ead556bd96abab6
alberto-paparella <alberto.paparella@edu.unife.it>
2024-01-25 11:55:33 +0100

    Added tests

--- test/propositionalfuzzysat.jl
+++ test/propositionalfuzzysat.jl
@@ -63,11 +63,53 @@ myalgebra = @heytingalgebra (α,) (⊥, α) (α, ⊤)
 @test fuzzysat(parseformula("(p→⊥)→p"), myalgebra) == true
 @test fuzzysat(parseformula("(p→⊥)→(p→⊥)"), myalgebra) == true
 
-@test fuzzysat(parseformula("(x∨y∨z)∧(x∨y∨(z→⊥))∧(x∨(y→⊥)∨z)∧(x∨(y→⊥)∨(z→⊥))∧((x→⊥)∨y∨z)∧((x→⊥)∨y∨(z→⊥))∧((x→⊥)∨(y→⊥)∨z)∧((x→⊥)∨(y→⊥)∨z)"), booleanalgebra) == true
-@test fuzzysat(parseformula("(x∨y∨z)∧(x∨y∨(z→⊥))∧(x∨(y→⊥)∨z)∧(x∨(y→⊥)∨(z→⊥))∧((x→⊥)∨y∨z)∧((x→⊥)∨y∨(z→⊥))∧((x→⊥)∨(y→⊥)∨z)∧((x→⊥)∨(y→⊥)∨(z→⊥))"), booleanalgebra) == false
+@test fuzzysat(
+    parseformula(
+        "(x∨y∨z)∧(x∨y∨(z→⊥))∧(x∨(y→⊥)∨z)∧(x∨(y→⊥)∨(z→⊥))∧" *
+        "((x→⊥)∨y∨z)∧((x→⊥)∨y∨(z→⊥))∧((x→⊥)∨(y→⊥)∨z)∧((x→⊥)∨(y→⊥)∨z)"
+    ),
+    booleanalgebra
+) == true
 
-@test fuzzysat(parseformula("(x∨y∨z)∧(x∨y∨(z→⊥))∧(x∨(y→⊥)∨z)∧(x∨(y→⊥)∨(z→⊥))∧((x→⊥)∨y∨z)∧((x→⊥)∨y∨(z→⊥))∧((x→⊥)∨(y→⊥)∨z)∧((x→⊥)∨(y→⊥)∨z)"), myalgebra) == true
-@test fuzzysat(parseformula("(x∨y∨z)∧(x∨y∨(z→⊥))∧(x∨(y→⊥)∨z)∧(x∨(y→⊥)∨(z→⊥))∧((x→⊥)∨y∨z)∧((x→⊥)∨y∨(z→⊥))∧((x→⊥)∨(y→⊥)∨z)∧((x→⊥)∨(y→⊥)∨(z→⊥))"), myalgebra) == false
+@test fuzzysat(
+    parseformula(
+        "(x∨y∨z)∧(x∨y∨(z→⊥))∧(x∨(y→⊥)∨z)∧(x∨(y→⊥)∨(z→⊥))∧" *
+        "((x→⊥)∨y∨z)∧((x→⊥)∨y∨(z→⊥))∧((x→⊥)∨(y→⊥)∨z)∧((x→⊥)∨(y→⊥)∨(z→⊥))"
+    ),
+    booleanalgebra
+) == false
 
-@test fuzzysat(booleantofuzzy(dimacstosole("benchmark/sat/uf50-01.cnf")), booleanalgebra) == true
-@test fuzzysat(booleantofuzzy(dimacstosole("benchmark/sat/uf50-01.cnf")), myalgebra) == true
+@test fuzzysat(
+    parseformula(
+        "(x∨y∨z)∧(x∨y∨(z→⊥))∧(x∨(y→⊥)∨z)∧(x∨(y→⊥)∨(z→⊥))∧" *
+        "((x→⊥)∨y∨z)∧((x→⊥)∨y∨(z→⊥))∧((x→⊥)∨(y→⊥)∨z)∧((x→⊥)∨(y→⊥)∨z)"
+    ),
+    myalgebra
+) == true
+
+@test fuzzysat(
+    parseformula(
+        "(x∨y∨z)∧(x∨y∨(z→⊥))∧(x∨(y→⊥)∨z)∧(x∨(y→⊥)∨(z→⊥))∧" *
+        "((x→⊥)∨y∨z)∧((x→⊥)∨y∨(z→⊥))∧((x→⊥)∨(y→⊥)∨z)∧((x→⊥)∨(y→⊥)∨(z→⊥))"
+    ),
+    myalgebra
+) == false
+
+@test booleantofuzzy(
+        parseformula(
+            "(x∨y∨z)∧(x∨y∨¬z)∧(x∨¬y∨z)∧(x∨¬y∨¬z)∧(¬x∨y∨z)∧(¬x∨y∨¬z)∧(¬x∨¬y∨z)∧(¬x∨¬y∨¬z)"
+        )
+    ) == parseformula(
+        "(x∨y∨z)∧(x∨y∨(z→⊥))∧(x∨(y→⊥)∨z)∧(x∨(y→⊥)∨(z→⊥))∧" *
+        "((x→⊥)∨y∨z)∧((x→⊥)∨y∨(z→⊥))∧((x→⊥)∨(y→⊥)∨z)∧((x→⊥)∨(y→⊥)∨(z→⊥))"
+    )
+
+@test fuzzysat(
+    booleantofuzzy(dimacstosole("benchmark/sat/uf50-01.cnf")),
+    booleanalgebra
+) == true
+
+@test fuzzysat(
+    booleantofuzzy(dimacstosole("benchmark/sat/uf50-01.cnf")),
+    myalgebra
+) == true
