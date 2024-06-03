b4ccdebd61868734a6071b9aa34309b35c0b66f1
alberto-paparella <alberto.paparella@edu.unife.it>
2024-06-03 11:18:11 +0200

    Updated mvhs reversal rules, began testing

--- test/mvhs.jl
+++ test/mvhs.jl
@@ -3,79 +3,31 @@ using SoleLogics.ManyValuedLogics
 using SoleReasoners
 using Test
 
-using SoleReasoners: Point, AFSLOS, checkafslos, mvhsalphasat
+include("mvhs/afslos.jl")
 
-using SoleLogics.ManyValuedLogics: α, β, H4
+include("mvhs/mvhsalphasat.jl")
 
-u, v = Point.(['u', 'v'])
-afslos1 = AFSLOS(
-    [u, v],
-    FiniteHeytingAlgebra(H4),
-    Dict(
-        (u,u) => ⊥, (u,v) => α,
-        (v,u) => β, (v,v) => ⊥
-    ),
-    Dict(
-        (u,u) => ⊤, (u,v) => ⊥,
-        (v,u) => ⊥, (v,v) => ⊤
-    )
-)
+include("mvhs/mvhsalphaprove.jl")
 
-let err = nothing
-    try
-        checkafslos(afslos1)
-    catch err
-    end
+# p, q = Atom.(["p", "q"])
+# diamondA = diamond(IA_A)
+# boxA = box(IA_A)
 
-    @test sprint(showerror, err)  == "(D,<,=) is not a AFSLOS (5)"
-end
-
-using SoleLogics.ManyValuedLogics: G3
-
-w = Point('w')
-afslos2 = AFSLOS(
-    [u, v, w],
-    FiniteHeytingAlgebra(G3),
-    Dict(
-        (u,u) => ⊥, (u,v) => ⊤, (u,w) => α,
-        (v,u) => ⊥, (v,v) => ⊥, (v,w) => ⊤,
-        (w,u) => ⊥, (w,v) => ⊥, (w,w) => ⊥
-    ),
-    Dict(
-        (u,u) => ⊤, (u,v) => ⊥, (u,w) => ⊥,
-        (v,u) => ⊥, (v,v) => ⊤, (v,w) => ⊥,
-        (w,u) => ⊥, (w,v) => ⊥, (w,w) => ⊤
-    )
-)
-
-let err = nothing
-    try
-        checkafslos(afslos2)
-    catch err
-    end
-
-    @test sprint(showerror, err)  == "(D,<,=) is not a AFSLOS (4)"
-end
-
-p, q = Atom.(["p", "q"])
-diamondA = diamond(IA_A)
-boxA = box(IA_A)
-
-@test mvhsalphasat(
-    ⊥,
-    ∧(diamondA(p), boxA(→(p, ⊥))),
-    FiniteHeytingAlgebra(G3),
-    verbose=false
-) == true
-@test mvhsalphasat(
-    α,
-    ∧(diamondA(p), boxA(→(p, ⊥))),
-    FiniteHeytingAlgebra(G3),
-    verbose=false
-) == false
-@test mvhsalphasat(
-    ⊤,
-    ∧(diamondA(p), boxA(→(p, ⊥))),
-    FiniteHeytingAlgebra(G3),
-    verbose=false
-) == false
+# @test mvhsalphasat(
+#     ⊥,
+#     ∧(diamondA(p), boxA(→(p, ⊥))),
+#     FiniteHeytingAlgebra(G3),
+#     verbose=false
+# ) == true
+# @test mvhsalphasat(
+#     α,
+#     ∧(diamondA(p), boxA(→(p, ⊥))),
+#     FiniteHeytingAlgebra(G3),
+#     verbose=false
+# ) == false
+# @test mvhsalphasat(
+#     ⊤,
+#     ∧(diamondA(p), boxA(→(p, ⊥))),
+#     FiniteHeytingAlgebra(G3),
+#     verbose=false
+# ) == false
