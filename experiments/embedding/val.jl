506b99c78812389152f100a12dc04d083bead089
Alberto Paparella <alberto.paparella@unife.it>
2025-09-26 15:08:38 +0200

    Updated embedding experiments; mlton currently not installed in CI (tests fail)

--- experiments/embedding/val.jl
+++ experiments/embedding/val.jl
@@ -1,6 +1,8 @@
 using Graphs, Random, SoleLogics, StatsBase, Test
 # using TikzGraphs, TikzPictures
 
+using SoleReasoners: val, installspartacus, sval
+
 installspartacus()
 
 rng = Xoshiro(42)
@@ -11,8 +13,9 @@ aot = vcat(Σ, [⊤, ⊥])
 aotpicker = (rng)->StatsBase.sample(rng, aot, StatsBase.uweights(length(aot)))
 
 na = 5
+maxw = 10
 e = Vector{KripkeStructure}()
-for nw in 2:10
+for nw in 2:maxw
     for ne in nw-1:nw*(nw-1)
         g = SimpleDiGraph(nw,ne)
         rem_vertices!(g, vertices(g)[map(v->!has_path(g,1,v),vertices(g))])
@@ -34,18 +37,24 @@ for nw in 2:10
     end
 end
 
-println("VAL")
-tp = 0
-fp = 0
-tn = 0
-fn = 0
-te = 0
-ts = 0
-minh = 6
+println("\nVALIDITY")
+tot_tp = 0
+tot_fp = 0
+tot_tn = 0
+tot_fn = 0
+tot_te = 0
+tot_ts = 0
+minh = 1
 maxh = 10
-nφ = 10
+nφ = 500
 for h in minh:maxh
-    println("h=$h")
+    tp = 0
+    fp = 0
+    tn = 0
+    fn = 0
+    te = 0
+    ts = 0
+    println("\nh=$h")
     for i in 1:nφ
         φ = randformula(
             rng,
@@ -58,25 +67,35 @@ for h in minh:maxh
         )
         t0 = time_ns()
         re = val(φ, e)
-        global te += time_ns() - t0
+        te += time_ns() - t0
         t0 = time_ns()
         rs = sval(φ)
-        global ts += time_ns() - t0
+        ts += time_ns() - t0
         if rs
             if re
-                global tp += 1
+                tp += 1
             else
-                global fn += 1
+                fn += 1
             end
         else
             if re
-                global fp += 1
+                fp += 1
             else
-                global tn += 1
+                tn += 1
             end
         end
     end
+    global tot_tp += tp
+    global tot_fp += fp
+    global tot_tn += tn
+    global tot_fn += fn
+    global tot_te += te
+    global tot_ts += ts
+    println("TP: $tp\tFP: $fp\tTN: $tn\tFN: $fn")
+    println("Embdegging avg. time: $(te/nφ) ns")
+    println("Spartacus avg. time: $(ts/nφ) ns")
 end
-println("TP: $tp\tFP: $fp\tTN: $tn\tFN: $fn")
-println("Embdegging avg. time: $(te/((maxh-minh+1)*nφ)) ns")
-println("Spartacus avg. time: $(ts/((maxh-minh+1)*nφ)) ns")
+println("\nRESULTS:")
+println("TP: $tot_tp\tFP: $tot_fp\tTN: $tot_tn\tFN: $tot_fn")
+println("Embdegging avg. time: $(tot_te/((maxh-minh+1)*nφ)) ns")
+println("Spartacus avg. time: $(tot_ts/((maxh-minh+1)*nφ)) ns")
