b3265692d7a187de63240e61794cd1b01acc8d16
alberto-paparella <pprlrt@unife.it>
2025-10-20 13:45:21 +1000

    Fixed sat

--- src/embedding/embedding.jl
+++ src/embedding/embedding.jl
@@ -1,38 +1,42 @@
 using Base.Threads: @threads
-using SoleLogics: AnyWorld, SyntaxBranch, check
-using StaticArrays: SVector
+using SoleLogics: AnyWorld, Atom, BooleanTruth, SyntaxBranch, check
 
-function embed(φ::SyntaxBranch, e::E; memo::M) where {E<:AbstractVector, M<:AbstractDict}
-    return SVector{length(e), Bool}(map(m->check(φ, m, AnyWorld(); use_memo=memo[m], memo_max_height=2), e))
+function embed(φ::Union{Atom, BooleanTruth, SyntaxBranch}, e::E; memo::M) where {E<:AbstractVector, M<:AbstractDict}
+    return Vector{Bool}(map(m->check(φ, m, AnyWorld(); use_memo=memo[m], memo_max_height=2), e))
 end
 
-function sat(φ::SyntaxBranch, e::E; memo::M) where {E<:AbstractVector, M<:AbstractDict}
-    # return count(embed(φ, e; memo=memo)) > 0
-    @threads for m in e
+function sat(φ::Union{Atom, BooleanTruth, SyntaxBranch}, e::E; memo::M) where {E<:AbstractVector, M<:AbstractDict}
+    # r = false
+    # l = ReentrantLock();
+    # @threads for m in e
+    for m in e
         if check(φ, m, AnyWorld(); use_memo=memo[m], memo_max_height=2)
+            # lock(l) do
+                # r = true
+            # end
             return true
         end
     end
     return false
 end
 
-function unsat(φ::SyntaxBranch, e::E; memo::M) where {E<:AbstractVector, M<:AbstractDict}
+function unsat(φ::Union{Atom, BooleanTruth, SyntaxBranch}, e::E; memo::M) where {E<:AbstractVector, M<:AbstractDict}
     return !sat(φ, e; memo=memo)
 end
 
-function val(φ::SyntaxBranch, e::E; memo::M) where {E<:AbstractVector, M<:AbstractDict}
+function val(φ::Union{Atom, BooleanTruth, SyntaxBranch}, e::E; memo::M) where {E<:AbstractVector, M<:AbstractDict}
     return unsat(¬(φ), e; memo=memo)
 end
 
-function unval(φ::SyntaxBranch, e::E; memo::M) where {E<:AbstractVector, M<:AbstractDict}
+function unval(φ::Union{Atom, BooleanTruth, SyntaxBranch}, e::E; memo::M) where {E<:AbstractVector, M<:AbstractDict}
     return !val(φ, e; memo=memo)
 end
 
-function eqv(φ::SyntaxBranch, ψ::SyntaxBranch, e::E; memo::M) where {E<:AbstractVector, M<:AbstractDict}
+function eqv(φ::Union{Atom, BooleanTruth, SyntaxBranch}, ψ::Union{Atom, BooleanTruth, SyntaxBranch}, e::E; memo::M) where {E<:AbstractVector, M<:AbstractDict}
     return embed(φ, e; memo=memo) == embed(ψ, e; memo=memo)
 end
 
-function ent(φ::SyntaxBranch, ψ::SyntaxBranch, e::E; memo::M) where {E<:AbstractVector, M<:AbstractDict}
+function ent(φ::Union{Atom, BooleanTruth, SyntaxBranch}, ψ::Union{Atom, BooleanTruth, SyntaxBranch}, e::E; memo::M) where {E<:AbstractVector, M<:AbstractDict}
     a = embed(φ, e; memo=memo)
     b = embed(ψ, e; memo=memo)
     return (a .& b) == a
